7b9eb2f917ce4a75f4bba8f66c4408cf315c4cfc
莯凛 <mokurin000@gmail.com>
2026-05-31 14:57:33 +0800

    perf: increase channel buffer size

--- desktop-tidy-master/src/model.rs
+++ desktop-tidy-master/src/model.rs
@@ -46,7 +46,7 @@ impl Component for MainModel {
             },
         }
 
-        let (cmd_tx, cmd_rx) = crossfire::spsc::bounded_async::<DesktopCommand>(1024);
+        let (cmd_tx, cmd_rx) = crossfire::spsc::bounded_async::<DesktopCommand>(4096);
         let (restored_tx, restored_rx) = crossfire::oneshot::oneshot::<()>();
 
         window.set_icon_by_id(1)?;
